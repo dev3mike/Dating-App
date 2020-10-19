4e4f6fe154f895f34aee5f1e7198d95dafdb71aa
max-macbook <masoudx@gmail.com>
2020-10-19 21:30:19 +0200

    Register Controller Added

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public AccountController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            // Track Adding Proccess
+            context.Users.Add(user);
+
+            // Save to Database
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
